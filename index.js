01194b1fae8f2da009cb459c21504984b5b81c64
Adam Caron <adamwcaron@gmail.com>
2015-11-10 10:37:44 -0700

    Add notes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express'); // get express, the sinatra of JavaScript.
+const app = express(); // instantiate it
+const http = require('http').Server(app); // pass it to my http library
+const io = require('socket.io')(http); // library for websockets: jQuery for websockets -- it papers over chrome's, safari's, and firefox's implementations.
 
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static('public')); // grab all the files in the /public directory and serve them... this express server is just serving static assets
 
 app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
