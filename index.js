a4c18991dce8145dbc3f7b263185328b97e7ab98
Adam Caron <adamwcaron@gmail.com>
2015-11-10 11:19:18 -0700

    Append messages to the DOM

--- index.js
+++ index.js
@@ -14,18 +14,11 @@ app.get('/', function (req, res){
 
 io.on('connection', function (socket) {
   console.log('Someone has connected.');
-  // the connection happens
-  // emit a message. Socket.io includes the 'eventEmitter' library
-  // the socket will now send a message over
-  // to this indivudual browser (in scope as 'socket', which was passed in).
-
-  // lets shoot some stuff over the wire on a regular interval:
   var count = 0;
   var interval = setInterval(function () {
     socket.emit('message', {user: 'turingbot', text: 'Hello, world! ' + count++});
   }, 500);
 
-  // clean up after yourself! lest there be a terrible memory link.
   socket.on('disconnect', function () {
     clearInterval(interval);
   });
--- public/application.js
+++ public/application.js
@@ -6,4 +6,11 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
   console.log('Something came along on the "message" channel:', message);
+  $('.messages').append(message)
+});
+
+var $messages = $('.messages');
+
+socket.on('message', function (message) {
+  $messages.append(`<p>${message.user}: ${message.text}</p>`);
 });
